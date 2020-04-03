a4d92bd268b293d63c520bceb8510a3e338d8b5f
hikmatkhanse <hikmathanse@gmail.com>
2020-04-03 22:42:49 +0200

    remove the home page and logout page coming

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -1,29 +1,15 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
 import { Redirect } from "react-router-dom";
 // import AdminNavbarLinks from "./AdminNavbarLinks.jsx";
+import { fire, facebookProvider } from "components/Config/Fire";
+import { Route } from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.mobileSidebarToggle = this.mobileSidebarToggle.bind(this);
+    this.logout = this.logout.bind(this);
     this.state = {
       sidebarExists: false
     };
@@ -45,12 +31,25 @@ class Header extends Component {
     };
     document.body.appendChild(node);
   }
+  logout() {
+    fire
+      .auth()
+      .signOut()
+      .then(function() {
+        /*  Route.history.push("/"); */
+        console.log("sign out success");
+      })
+      .catch(function(error) {
+        console.log("error");
+      });
+  }
   render() {
     return (
       <Navbar fluid>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#pablo">{this.props.brandText}</a>
+            <button onClick={this.logout}>Logout</button>
           </Navbar.Brand>
 
           <Navbar.Toggle onClick={this.mobileSidebarToggle} />
